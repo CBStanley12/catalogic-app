0c8b723d6a363b57818a7f9a27f5fdd4c1009881
Christopher Stanley <cbstanley12@gmail.com>
2019-03-29 22:37:52 -0600

    Initial commit

---
+++ project.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask import session as login_session
+import random, string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+from oauth2client.client import AccessTokenCredentials
+import httplib2
+import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+
+""" CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Item Catalog Application"
+
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+Base.metadata.bind = engine """
+
+""" DBSession = sessionmaker(bind=engine)
+session = DBSession() """
+
+@app.route('/')
+@app.route('/categories/')
+def showCategories():
+    return render_template('categories.html')
+
+if __name__ == '__main__':
+    # app.secret.key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8000)
+
