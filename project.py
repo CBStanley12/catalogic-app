52a4e3dc30b76ccdb481d86758875669ff930e4b
Christopher Stanley <cbstanley12@gmail.com>
2019-03-30 19:54:13 -0600

    Added pages and styles to view categories

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, Item
 from flask import session as login_session
 import random, string
 from oauth2client.client import flow_from_clientsecrets
@@ -13,19 +14,20 @@ import requests
 
 app = Flask(__name__)
 
-""" CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
-APPLICATION_NAME = "Item Catalog Application"
+# CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Animal Catalog Application"
 
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
-Base.metadata.bind = engine """
+engine = create_engine('sqlite:///item_catalog.db')
+Base.metadata.bind = engine
 
-""" DBSession = sessionmaker(bind=engine)
-session = DBSession() """
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/categories/')
 def showCategories():
-    return render_template('categories.html')
+    categories = session.query(Category).order_by(Category.name)
+    return render_template('categories.html', categories = categories)
 
 if __name__ == '__main__':
     # app.secret.key = 'super_secret_key'
